78479fc44c1528b56e6d378f3ccf5e0fd9e98d90
Bryan Kassulke <bryan.kassulke@axcelerate.com>
2024-10-25 09:59:10 +0100

    vite init

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # bryankassulke.github.io
+
+Welcome to my website. It's a work in progress so come back later :^)
---
+++ main.js
@@ -0,0 +1,7 @@
+document.querySelector('#app').innerHTML = /* html */`
+  <div>
+    <a href="https://github.com/BryanKassulke" target="_blank">
+      Breehee
+    </a>
+  </div>
+`;
